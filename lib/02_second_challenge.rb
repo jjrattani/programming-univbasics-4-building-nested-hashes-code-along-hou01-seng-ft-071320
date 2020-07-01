b40d1b7aa573a5418e61b850f690a16a63edfc31
Jessica  Rattani <jjrattani@gmail.com>
2020-07-01 13:54:24 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {:name "Lord Montague", :age: "53"}, 
-      :matriarch {:name => "Lady Montague", :age => "54"},
-      :hero {:name => :Romeo, :age => "15"}, 
-      :hero_friends [:name => Benolio", :age: "17", :attitude => "hot-headed"},]
+      :patriarch => {}, 
+      :matriarch {},
+      :hero {}, 
+      :hero_friends []
   },
     :capulet => {
       #your key/value pairs here!
