5f53c1dc848baa35eafca51a1350223f0a6b9cfa
Jessica  Rattani <jjrattani@gmail.com>
2020-07-01 13:18:31 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,6 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+    :montague => {:patriarch {:name "Lord Montague" => :age: 53} , :matriarch {:name => "Lady Montague =>"}, : :hero {}, :hero_friends []},
+    :capulet ==> {:patriarch {} , :matriarch {}, : :hero {}, :hero_friends []}
+   } 
 end
